b65eeeeaf2ae5fc1430bedaba166e4d4b159fc5b
Andre Perez <andremztm@gmail.com>
2023-08-09 10:28:59 -0300

    Initial commit

---
+++ example/views.py
@@ -0,0 +1,15 @@
+from django.http import HttpResponse
+from django.utils import timezone
+
+
+def index(request):
+    now = timezone.now()
+    html = f'''
+    <html>
+        <body>
+            <h1>Hello from Vercel!</h1>
+            <p>The current time is { now }.</p>
+        </body>
+    </html>
+    '''
+    return HttpResponse(html)
